7d6f6649bf34648e7974b11c0d5566f7035e35fd
dlwodn1414 <dlwodn1414@mindbloom.co.kr>
2024-08-19 17:33:08 +0900

    수정

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+function create(baseURL, options) {
+	const instance = axios.create(Object.assign({ baseURL }, options));
+
+	return instance;
+}
+
+export const posts = create('http://localhost:5000/posts/');
--- src/router/index.js
+++ src/router/index.js
@@ -46,10 +46,10 @@ const routes = [
 		path: '/posts/:id',
 		name: 'PostDetail',
 		component: PostDetailView,
-		// props: true,
-		props: route => ({
-			id: parseInt(route.params.id),
-		}),
+		props: true,
+		// props: route => ({
+		// 	id: parseInt(route.params.id),
+		// }),
 	},
 	{
 		path: '/posts/:id/edit',
